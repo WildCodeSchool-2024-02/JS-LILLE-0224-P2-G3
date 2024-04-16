65838f4e23b9f7bdd0343dbe111be2672adc23a1
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-04-16 17:18:44 +0200

    add onclick to get the gender category

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -3,27 +3,42 @@ import Api from "./Api";
 import "./Categories.scss";
 
 function Categories() {
+  //    SHOW THE DIFFERENT CATEGORIES
   const [categorie, setCategories] = useState([]);
-
 
   const getCategories = () => {
     Api.getCategories.then((resp) => {
       setCategories(resp.data.results);
     });
-  }
-
+  };
 
   useEffect(() => {
     getCategories();
   }, []);
 
+  //    GET THE GENDER WHEN YOU CLICK
+  // LIGNES DU DESSOUS COMMENTÉES POUR PUSH DE 21 À 39
+  // const [gamesByGenreId, setGamesByGenreId] = useState([]);
+
+  // const getGamesbyGenreId = (id) => {
+  //   Api.getGamesbyGenreId(id).then((resp) => {
+  //     setGamesByGenreId(resp.data.results);
+  //   });
+  // };
 
   return (
     <div>
       <h2> genre</h2>
       <div className="grid_categorie_card">
         {categorie.map((item) => (
-          <div className="categorie_card" key="{item.name}">
+          <div
+            role="presentation"
+            key={`${item.name}-${item.id}`}
+            // onClick={() => {
+            //   getGamesbyGenreId(item.id);
+            // }}
+            className="categorie_card"
+          >
             <h4 className="categorie_name">{item.name}</h4>
             <img
               className="image_categorie"
