d20cb11b08a38078dc3eff8d2bd9548043bed312
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-04-17 09:50:04 +0200

    add of the api.jsx with the base url related to and App.jsx where the function search is called

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Categories from "./components/Categories/Categories";
+import Search from "./components/Search/Search";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
    
     <div>
       
+    <Search />
     <Categories/>
     </div>
     
