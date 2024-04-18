ad0906ed172c05a0b3214fb7bed7d34144a56444
Your Name <you@example.com>
2024-04-18 09:54:00 +0200

    Add images separator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,31 @@ import CardStreamList from "./components/Stream/CardStreamList";
 import Trends from "./components/Trends/Trends";
 import Categories from "./components/Categories/Categories";
 import Search from "./components/Search/Search";
+import "../public/backgroundimages/imagetop.png";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <div className="image_frame">
+        <img
+          className="imageTop"
+          src="../public/backgroundimages/imagetop.png"
+          alt="imagetop"
+        />
+      </div>
       <Trends />
       <CardStreamList />
-    <Search />
-    <Categories/>
+      <Search />
+      <div className="image_frame">
+        <img
+          className="imageTop"
+          src="../public/backgroundimages/imagebot1.png"
+          alt="imagebot"
+        />
+      </div>
+
+      <Categories />
     </>
   );
 }
