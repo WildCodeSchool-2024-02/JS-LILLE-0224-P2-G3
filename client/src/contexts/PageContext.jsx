4ff70bcca2b14c4fa6b5e039801f775a9f57e274
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-04-25 16:13:11 +0200

    correction sur le children qui ne passait pas avec eslint pour le context provide de la pagination

--- client/src/contexts/PageContext.jsx
+++ client/src/contexts/PageContext.jsx
@@ -1,14 +1,16 @@
-import {
-    createContext, useContext, useMemo, useState, useCallback
-  } from "react";
+import  { createContext, useContext, useMemo, useState, useCallback } from "react";
+import PropTypes from 'prop-types';
 
-  const PageContext = createContext ();
+const PageContext = createContext();
 
-  export function PageProvider ({
-    // eslint-disable-next-line react/prop-types
-    children
-  }) {
+
+export function PageProvider({ children }) {
     const [page, setPage] = useState(1);
+
+PageProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
     const NexthandleClick = useCallback(() => {
         setPage(page + 1);
     }, [page]);
@@ -18,16 +20,14 @@ import {
             setPage(page - 1);
         }
     }, [page]);
+
     const CurrentPage = useMemo(() => ({ page, setPage, NexthandleClick, PrevioushandleClick }), [page, NexthandleClick, PrevioushandleClick]);
 
     return (
-        <PageContext.Provider
-          
-          value={CurrentPage}
-        >
-         {children}
-       </PageContext.Provider>
-     );
-  }
-  export const usePage =
-    () => useContext(PageContext);
+        <PageContext.Provider value={CurrentPage}>
+            {children}
+        </PageContext.Provider>
+    );
+}
+
+export const usePage = () => useContext(PageContext);
