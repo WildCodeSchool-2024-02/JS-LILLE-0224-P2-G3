0fe6ec71f08d73face9a7048ff4ef5f7a04c0ef3
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-04-25 16:51:15 +0200

    correction de la position des proptypes dans pagecontext et modification de deux images/logo pour la couleur

--- client/src/contexts/PageContext.jsx
+++ client/src/contexts/PageContext.jsx
@@ -7,27 +7,26 @@ const PageContext = createContext();
 export function PageProvider({ children }) {
     const [page, setPage] = useState(1);
 
-PageProvider.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-
+    
     const NexthandleClick = useCallback(() => {
         setPage(page + 1);
     }, [page]);
-
+    
     const PrevioushandleClick = useCallback(() => {
         if (page > 1) {
             setPage(page - 1);
         }
     }, [page]);
-
+    
     const CurrentPage = useMemo(() => ({ page, setPage, NexthandleClick, PrevioushandleClick }), [page, NexthandleClick, PrevioushandleClick]);
-
+    
     return (
         <PageContext.Provider value={CurrentPage}>
             {children}
         </PageContext.Provider>
     );
 }
-
+PageProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 export const usePage = () => useContext(PageContext);
