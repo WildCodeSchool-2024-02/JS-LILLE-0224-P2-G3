b2a7dca90fff2a0a063dace1e6dda1cae1f342cb
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-04-25 14:29:09 +0200

    API KEY in .env file

--- client/src/components/Categories/Api.jsx
+++ client/src/components/Categories/Api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const key = "fb5c5861fcf14a6c906d2a2ee9ad282b";
+const key = import.meta.env.API_KEY;
+
 const axiosCreate = axios.create({
   baseURL: "https://api.rawg.io/api",
 });
@@ -10,12 +11,11 @@ const getCategories = axiosCreate.get(`/genres?key=${key}`);
 const getByName = (search) =>
   axiosCreate.get(`/games?search=${search}&key=${key}`);
 
-const getGamesByCategory = (idGenre) =>
+const getGamesByGenre = (idGenre) =>
   axiosCreate.get(`/games?genres=${idGenre}&key=${key}`);
 
 export default {
   getCategories,
   getByName,
-  getGamesByCategory,
-
+  getGamesByGenre,
 };
