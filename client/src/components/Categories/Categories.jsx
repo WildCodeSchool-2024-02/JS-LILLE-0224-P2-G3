ebc7d427acd742957834d5871264bb5852ed4b42
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-04-26 10:00:00 +0200

    correction pr

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Api from "./Api";
+import Api from "../../services/Api";
 import "./Categories.scss";
 
 function Categories() {
--- client/src/components/CategoryPage/CategoryCard.jsx
+++ client/src/components/CategoryPage/CategoryCard.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Api from "../Categories/Api";
+import Api from "../../services/Api";
 import FilterCategoryPlatform from "./FilterCategoryPlatform";
 import "./CategoryCard.scss";
 
