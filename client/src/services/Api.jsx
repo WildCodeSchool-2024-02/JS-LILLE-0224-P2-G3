871e5b0061833ad1a1456b857de37f306b0a19eb
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-04-26 09:37:42 +0200

    correction pr

---
+++ client/src/services/Api.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const key = import.meta.env.VITE_API_KEY;
+const axiosCreate = axios.create({
+  baseURL: "https://api.rawg.io/api",
+});
+
+const getCategories = axiosCreate.get(`/genres?key=${key}`);
+
+const getByName = (search) =>
+  axiosCreate.get(`/games?search=${search}&key=${key}`);
+
+const getGamesByGenre = (idGenre) =>
+  axiosCreate.get(`/games?genres=${idGenre}&key=${key}`);
+
+export default {
+  getCategories,
+  getByName,
+  getGamesByGenre,
+};
