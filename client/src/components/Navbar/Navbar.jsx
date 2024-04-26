9bf35562a1ee9009764ad108cac313557130041a
PMorganGit <morganpouilly@gmail.com>
2024-04-26 11:37:51 +0200

    navbar css responsive & fixed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Search from "../Search/Search";
 import "./Navbar.scss";
 
-
 function Navbar() {
   const [isSearchVisible, setSearchVisible] = useState(false);
 
@@ -11,7 +10,6 @@ function Navbar() {
   };
 
   const scrollToSection = (id) => {
-
     const section = document.getElementById(id);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
@@ -22,47 +20,47 @@ function Navbar() {
       <div className="navbar">
         <div className="logo_titre_navbar">
           <div className="imglogo_navbar">
-            <img className="image_navbar_logo" src="../public/logo/logo-final.png" alt="logo" />
+            <img
+              className="image_navbar_logo"
+              src="../public/logo/logo-final.png"
+              alt="logo"
+            />
           </div>
         </div>
         <div className="anchor_filter_navbar">
           <div className="anchor_navbar">
             <ul>
-              <li>
-                <a
-                  href="#trending"
-                  onClick={() => scrollToSection("trends")}
-                >
+              <li className="li_">
+                <a href="#trending" onClick={() => scrollToSection("trends")}>
                   Tendances
                 </a>
-
               </li>
               <li>
-                <a
-                  href="#streaming"
-                  onClick={() => scrollToSection("stream")}
-                >Streaming
+                <a href="#streaming" onClick={() => scrollToSection("stream")}>
+                  Streaming
                 </a>
               </li>
               <li>
-                <a
-                  href="#category"
-                  onClick={() => scrollToSection("categ")}
-                >Catégories
+                <a href="#category" onClick={() => scrollToSection("categ")}>
+                  Catégories
                 </a>
               </li>
-              <li>
-                <button type="button" onClick={toggleSearch}>
-                  <img className="search_logo" src="../../../public/button/rechercher.png" alt="rechercher" />
-                </button>
+              <li className="li_button">
+                <div className="button_search">
+                  <button type="button" onClick={toggleSearch}>
+                    <img
+                      className="search_logo"
+                      src="../../../public/button/rechercher.png"
+                      alt="rechercher"
+                    />
+                  </button>
+                </div>
               </li>
             </ul>
+            <div className="search_navbar">{isSearchVisible && <Search />}</div>
           </div>
-
-          {isSearchVisible && <Search />}
         </div>
       </div>
-
     </div>
   );
 }
